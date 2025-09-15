e13febf5beecefb851054db70eb71a516e200462
a0909123137 <a0909123137@gmail.com>
2025-09-15 16:26:00 +0800

    第三次commit(加入按鈕)

---
+++ app/src/main/java/s1131246tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -0,0 +1,48 @@
+package s1131246tw.edu.pu.csim.tcyang.lotto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import s1131246tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LottoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Play(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Play(modifier: Modifier = Modifier) {
+
+    var lucky = (1..100).random()
+
+    Text(
+        text = "樂透數字(1-100)為 $lucky",
+        modifier = modifier
+    )
+
+    Button (
+        onClick = {lucky = (1..100).random()}
+    ) {
+        Text("重新產生樂透碼")
+    }
+}
+
